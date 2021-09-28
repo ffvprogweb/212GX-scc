1a64db17df65da5c9a365ce059b5623044376b66
profe <profe@192.168.0.38>
2021-09-28 10:31:20 -0300

    versao crud

--- scc/src/main/java/com/fatec/scc/controller/HomeController.java
+++ scc/src/main/java/com/fatec/scc/controller/HomeController.java
@@ -6,23 +6,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-	@GetMapping("/")
-	public ModelAndView menu() {
-		return new ModelAndView("paginaMenu");
-	}
-
 	@GetMapping("/login")
 	public ModelAndView autenticacao() {
 		return new ModelAndView("paginaLogin");
 	}
 
-	@GetMapping("/clientes")
-	public ModelAndView cadastrarCliente() {
-		return new ModelAndView("cadastrarCliente");
-	}
-
-	@GetMapping("/fornecedores")
-	public ModelAndView cadastrarFornecedor() {
-		return new ModelAndView("cadastrarFornecedor");
+	@GetMapping("/")
+	public ModelAndView home() {
+		return new ModelAndView("paginaMenu");
 	}
 }
