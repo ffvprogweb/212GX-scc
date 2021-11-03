ea4344691a7ca21ef0e42a2337e2efb9efea4a85
Edson Saraiva de Almeida <68782201+ffvprogweb@users.noreply.github.com>
2021-11-03 15:43:00 -0300

    Refatoração - inclusao da autenticação utilizando JPA

---
+++ scc/src/main/java/com/fatec/scc/model/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.fatec.scc.model;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleRepository {
+
+}
---
+++ scc/src/main/java/com/fatec/scc/model/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.fatec.scc.model;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface UsuarioRepository extends CrudRepository <Usuario, String> {
+	Usuario findByLogin(String login);
+}
